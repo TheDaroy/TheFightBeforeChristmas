e3f84d01ccb2c6e4bf6ed110e56f46a6a58d1f3f
daroy <thedaroy@live.com>
2019-12-06 22:40:18 +0100

    HIII

---
+++ Assets/Scripts/Spawnlocations.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnlocations : MonoBehaviour
+{
+    public GameObject[] spawnLocation;
+    public GameObject enemy;
+
+
+    private void Start()
+    {
+        spawnLocation = new GameObject[] { };
+    }
+
+
+}
