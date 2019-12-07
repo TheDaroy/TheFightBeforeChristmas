eb792236b1579d87412c9123dc4799f0e4300210
daroy <thedaroy@live.com>
2019-12-07 03:57:11 +0100

    no message

--- Assets/Scripts/TurretPlatform.cs
+++ Assets/Scripts/TurretPlatform.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class TurretPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float cost;
+    player Player;
+    GameObject spawnLocation;
+    GameObject turret;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            Player = collision.gameObject.GetComponent<player>();
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            Player = null;
+        }
+    }
+    private void Update()
+    {
+        if (Player && Input.GetKeyDown(KeyCode.X) && Player.goldAmount >= cost)
+        {
+            GameObject tamp = Instantiate(turret, spawnLocation.transform.position, spawnLocation.transform.rotation);
+
+        }
     }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class player : entity
 {
     [SerializeField] private LayerMask terrainLayer;
+    public int goldAmount = 0;
     void Start()
     {
 
